6f92edce9a6fe60dd12a84467290c795b728f336
MoophHD <d_sizykh@mail.ru>
2018-01-12 12:10:42 +0300

    pods, force direction and release

--- Assets/scripts/Game/Pod.cs
+++ Assets/scripts/Game/Pod.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pod : MonoBehaviour {
-    private float force;
+    private Rigidbody2D rb;
+    private float force = 10f;
     private LineRenderer lr;
 
-    private Vector3 swipeStart;
-    private Vector3 swipeFinish;
 
+    private bool swiping = false;
+    private float radius = 1.5f;
+    private Vector3 direction = Vector3.zero;
+    private Vector3 anchor;
+    // private Vector3 swipeStart = Vector3.zero;
+    // private Vector3 swipeFinish = Vector3.zero;
+    
     void DrawLine(Vector3 start, Vector3 end, Color cl)
     {
         GameObject myLine = new GameObject();
@@ -17,6 +23,7 @@ public class Pod : MonoBehaviour {
         myLine.AddComponent<LineRenderer>();
         lr = myLine.GetComponent<LineRenderer>();
         lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.sortingOrder = 1;
 		lr.startColor = cl;
 		lr.endColor = cl;
 		lr.startWidth = 0.1f;
@@ -26,23 +33,58 @@ public class Pod : MonoBehaviour {
     }
 
     void Start() {
-        DrawLine(Vector3.zero, Vector3.zero, Color.red);
+        rb = GetComponent<Rigidbody2D>();
+        // DrawLine(Vector3.zero, Vector3.zero, Color.red);
+        anchor = GetComponent<Transform>().position;
+        DrawLine(anchor, anchor, Color.red);
+        
+    }
+
+    void OnEnable () {
+        GameEvents.listen("UNFREEZE", handleUnfreeze);
+        
+    }
+
+    void OnDisable  () {
+        GameEvents.stopListening("UNFREEZE", handleUnfreeze);
+    }
+
+    void handleUnfreeze () {
+        jump();
+        hideLine();
+    }
+
+    void jump() {
+        rb.AddForce(new Vector2(direction.x, direction.y) * force, ForceMode2D.Impulse);
+    }
+
+    void hideLine() {
+        GameObject.Destroy(lr);
     }
 
     void Update() {
-        // SWIPE START
-        if (Input.touchCount == 1) {
-            swipeStart = Input.GetTouch(0).position;
-            lr.SetPosition(0, swipeStart);
+        // PRESS
+        if (Input.GetMouseButtonDown(0)) {
+            if (!swiping) {
+                swiping = true;
+            }
+        }
+        //HOLD
+        if (swiping) {
+            Vector3 finPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            finPoint.z = 0;
+            // print(absolutePoint);
+            // direction = absolutePoint.normalized;
+            // Vector3 clampedPoint = direction * radius;
+
+            direction = (finPoint - anchor).normalized;
+      
+            // lr.SetPosition(1, (direction + anchor) * radius );
+            lr.SetPosition(1, Vector3.ClampMagnitude(direction, radius)+ anchor );
         }
 
-        // SWIPING
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            swipeFinish = Input.GetTouch(0).position;
-            lr.SetPosition(1, swipeFinish);
-            
+        if (Input.GetMouseButtonUp(0)) {
+            swiping = false;
         }
     }
 }
